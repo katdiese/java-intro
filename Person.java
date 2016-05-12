c022d0880fbd2c0a002e717b5154b792072d9986
Kathy Dieser <katdiese@gmail.com>
2016-05-11 18:08:39 -0600

    created Person class

---
+++ Person.java
@@ -0,0 +1,19 @@
+public class Person {
+  public String first;
+  public String last;
+
+  public Person(String first, String last) {
+    this.first = first;
+    this.last = last;
+  }
+
+  public String fullName() {
+    return first + " " + last;
+  }
+
+  public static void main(String [] args) {
+    Person rambo = new Person("Sylvester", "Stallone");
+    rambo.fullName();
+  }
+
+}
